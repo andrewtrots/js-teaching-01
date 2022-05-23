b8dce7177323cc5690802d967cd6db737ab8ef7e
Andrew <andrewtrots@gmail.com>
2022-05-23 20:07:55 +0300

    add test.js(new tasks)

---
+++ js/test.js
@@ -0,0 +1,13 @@
+//part 1
+const doubleArray = arr => arr.map(el => el * 2);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const doubleArr = doubleArray(arr);
+console.log(doubleArr);
+
+
+//part 2
+function helloWorld() {
+  const str = 'Hello World!';
+  console.log(str);
+}
+helloWorld();
